ee582e63b559811007967302262b8f8a0413636d
Ayubxon <ayubxon1775@mail.ru>
2024-03-27 11:23:00 +0500

    added text

---
+++ server/src/routes/index.mjs
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+import mentorRouter from './mentor.mjs';
+import authRouter from '/auth-login.mjs';
+import pupilRouter from './pupils.mjs'
+
+
+const router = Router();
+
+router.use('/api', mentorRouter);
+router.use('/api', authRouter);
+router.use('/api', pupilRouter);
+
+export default router;
