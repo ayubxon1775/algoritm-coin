7b51a638a07de3feb2ea2727dd29edeb8c8f558c
Ayubxon <ayubxon1775@mail.ru>
2024-04-04 09:50:17 +0500

    first commit

--- client/src/components/home/home-page.jsx
+++ client/src/components/home/home-page.jsx
@@ -4,10 +4,12 @@ import { Saidbar, Main, Navbar} from '../'
 
 const HomePage = () => {
   return (
-    <div className="grid grid-cols-6 grid-rows-9 gap">
+    <div className="grid grid-cols-6 grid-rows-9 gap row-9">
             <Saidbar/>
             <Navbar/>
             <Main/>
+
+
     </div>
   )
 }
