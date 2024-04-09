5b70a36f101e77db3ff60d09f4c7ee3c940c7646
Ayubxon <ayubxonaxmatxonov@gmail.com>
2024-04-09 22:56:09 +0500

    first commit

--- client/src/components/pages/all-informations/all-information.jsx
+++ client/src/components/pages/all-informations/all-information.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
 
-const allInformation = () => {
-  return (
-   <div className='grid grid-cols-4 grid-rows-3 gap-8'>
-    <div className='card col-span-4 h-40 bg-white'>
-      <div className=''></div>
-      <div className=''></div>
-      <div className=''></div>
+
+
+const AllInformation = () => {
+
+return (
+  <div className="grid grid-cols-4 gap-10">
+    <div className="card col-span-4 h-40 flex items-center justify-between gap-4">
+      {cards.map(item => (
+        <div className="bg-white h-full w-[335px] rounded-lg p-4">
+          <div className="flex items-center justify-between mb-16">
+            <h1 className="text-2xl font-semibold text-slate-500">{item.title}</h1>
+            {<item.icon className="text-3xl text-slate-500"/>}
+          </div>
+        </div>
+      ))}
     </div>
-    <div className='mentors-col-span-2 bg-white'></div>
-    <div className='pupils col-span-2 bg-white'></div>
-    <div className='footer col-span-4 bg-white'></div>
-   </div>
-  )
-}
+  </div>
+)
 
-export default allInformation
+}
