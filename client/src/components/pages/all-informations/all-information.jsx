ddda2f9021e7d678ae0476ee83a11669c527abc1
Ayubxon <ayubxonaxmatxonov@gmail.com>
2024-04-29 19:20:55 +0500

    first commit

--- client/src/components/pages/all-informations/all-information.jsx
+++ client/src/components/pages/all-informations/all-information.jsx
@@ -10,7 +10,7 @@ const AllInformation = () => {
 
   return (
     <div className="grid grid-cols-4  gap-10">
-      <div className="card col-span-4 h-40 flex items-center justify-between">
+      <div className="card col-span-4 h-40 flex items-center justify-between gap-8">
         { cards.map(item => (
           <div className="bg-white h-full w-[280px] rounded-lg p-4">
             <div className="flex items-center justify-between mb-16">
--- client/src/components/pages/update-mentor/update-mentor.jsx
+++ client/src/components/pages/update-mentor/update-mentor.jsx
@@ -1,9 +1,90 @@
 import React from 'react'
-
-const updateMentor = () => {
+import { Card, Typography, Input,  IconButton,
+  Tooltip, } from "@material-tailwind/react";
+import { MagnifyingGlassIcon, PencilIcon, TrashIcon } from "@heroicons/react/24/outline";
+import { useTheme } from '../../../helpers/context';
+ 
+const TABLE_HEAD = ["UserName", "Phone Number", "Date", "Edits", "Deleted"];
+ 
+const UpdateMentor = () => {
+  const { handleSearch} = useTheme();
   return (
-    <div>update Mentor</div>
+    <div className='grid grid-cols-6 grid-rows-10 h-screen gap-4'>
+      <div className='col-span-6 row-span-1'>
+      <div className="w-[350px]">
+        <Input 
+          color='white' label="Search mentor..." 
+          icon={<MagnifyingGlassIcon className="h-5 w-5 text-gray-100"/>} 
+          className='text-gray-100 out' 
+          onChange={handleSearch}
+          />
+      </div>
+      </div>
+      <div className='col-span-3 row-span-9'><MentorsTable/></div>
+      <div className='col-span-3 row-span-9 border-[1px] border-gray-300'></div>
+    </div>
   )
 }
 
-export default updateMentor
+export default UpdateMentor
+ 
+function MentorsTable() {
+  const {filteredData} = useTheme();
+
+  return (
+    <Card className="h-full w-full overflow-scroll">
+      <table className="w-full min-w-max table-auto text-left">
+        <thead>
+          <tr>
+            {TABLE_HEAD.map((head) => (
+              <th key={head} className="border-b border-blue-gray-100 bg-blue-gray-50 p-4">
+                <Typography
+                  variant="small"
+                  color="blue-gray"
+                  className="font-normal leading-none opacity-70"
+                >
+                  {head}
+                </Typography>
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {filteredData.map(({ username, phoneNumber, createdAt }, index) => (
+            <tr key={username} className="even:bg-blue-gray-50/50">
+              <td className="p-4">
+                <Typography variant="small" color="blue-gray" className="font-normal">
+                  {username}
+                </Typography>
+              </td>
+              <td className="p-4">
+                <Typography variant="small" color="blue-gray" className="font-normal">
+                  {phoneNumber}
+                </Typography>
+              </td>
+              <td className="p-4">
+                <Typography variant="small" color="blue-gray" className="font-normal">
+                  {createdAt}
+                </Typography>
+              </td>
+              <td className="p-4">
+              <Tooltip content="Edit User">
+                <IconButton variant="text">
+                  <PencilIcon className="h-4 w-4" />
+                </IconButton>
+              </Tooltip>
+              </td>
+              <td className="p-4">
+              <Tooltip content="Edit User">
+                <IconButton variant="text">
+                  <TrashIcon className="h-4 w-4" />
+                </IconButton>
+              </Tooltip>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </Card>
+  );
+}
